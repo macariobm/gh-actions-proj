6982732fcc80a87d163cc0c995a16beafbeaea48
Macario Moura <macario.moura@gmail.com>
2024-03-18 10:32:27 -0300

    providers.tf with env variables

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "${AWS_REGION}" #hard-coded to sa-east-1
+  access_key = "${AWS_ACCES_KEY_ID}"
+  secret_key = "${AWS_SECRET_ACCESS_KEY}"
+
+}
