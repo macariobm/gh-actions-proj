4300c56fbe2d9d7b9ca91ad8590e2af3673e9968
Macario Moura <macario.moura@gmail.com>
2024-03-19 10:02:21 -0300

    fixed the script

--- fetch-ips.sh
+++ fetch-ips.sh
@@ -6,10 +6,10 @@ FILE=./hosts
 
 
 if [ -f "$FILE" ]; then
-  echo ${terraform output -json dev_env_ip} | jq -r '.[]' >> hosts
+  echo ${terraform output -json instance_ips} | jq -r '.[]' >> hosts
 else
   echo "[servers]" > hosts
-  echo ${terraform output -json dev_env_ip} | jq -r '.[]' >> hosts
+  echo ${terraform output -json instance_ips} | jq -r '.[]' >> hosts
 fi
 
 
