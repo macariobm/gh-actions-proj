6eaf02979f9e775ff8b02c358f2976308ef90323
Macario Moura <macario.moura@gmail.com>
2024-03-19 14:26:46 -0300

    added https ingress rule

--- main.tf
+++ main.tf
@@ -58,6 +58,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   cidr_ipv4         = "0.0.0.0/0"
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  description       = "alow https connection"
+  security_group_id = aws_security_group.sec_group.id
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_egress" {
   description                  = "alow all ingress"
   security_group_id            = aws_security_group.sec_group.id
